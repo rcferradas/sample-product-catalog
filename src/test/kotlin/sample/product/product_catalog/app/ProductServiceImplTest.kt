24bff028ddd46d524a21542d96e10c433d3ea5d1
Rodrigo Currás <rcferradas@esei.uvigo.es>
2025-03-14 12:48:55 +0100

    add basic testing for retrieving products and filtering by category

---
+++ src/test/kotlin/sample/product/product_catalog/app/ProductServiceImplTest.kt
@@ -0,0 +1,50 @@
+package sample.product.product_catalog.app
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import sample.product.product_catalog.domain.Product
+import sample.product.product_catalog.domain.ProductCategory
+import sample.product.product_catalog.infrastructure.database.ProductRepository
+
+@ExtendWith(MockitoExtension::class)
+class ProductServiceImplTest {
+ @InjectMocks private lateinit var service: ProductServiceImpl
+ private val repository: ProductRepository = mock()
+
+ @Test
+ fun `should return all products when no category is specified`() {
+  val products = listOf(
+   Product("SKU0001", "Wireless Mouse", 19.99, ProductCategory.ELECTRONICS),
+   Product("SKU0002", "4K TV", 499.00, ProductCategory.ELECTRONICS)
+  )
+  whenever(repository.findAll()).thenReturn(products)
+
+  val result = service.getProducts()
+
+  assertEquals(2, result.size)
+ }
+
+ @Test
+ fun `should return only products of category specified`() {
+  val products = listOf(
+   Product("SKU0001", "Wireless Mouse", 19.99, ProductCategory.ELECTRONICS),
+   Product("SKU0002", "4K TV", 499.00, ProductCategory.ELECTRONICS),
+   Product("SKU0003", "Water Bottle", 29.50, ProductCategory.HOME_KITCHEN)
+  )
+  whenever(repository.findByCategory(ProductCategory.ELECTRONICS.categoryName)).thenReturn(products.filter { it.category == ProductCategory.ELECTRONICS})
+
+  val result = service.getProducts(ProductCategory.ELECTRONICS)
+
+  verify(repository).findByCategory(ProductCategory.ELECTRONICS.categoryName)
+  assertEquals(2, result.size)
+  assertTrue(result.all { it.category == ProductCategory.ELECTRONICS.categoryName })
+ }
+
+}
